e51210b14aa2090b247946076bca159b4bcf41f3
Kevin Lara <kjlmenjivar@gmail.com>
2019-08-30 20:36:35 -0600

    Práctica 4

---
+++ Práctica_4/Práctica_4/Práctica 4_4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_4
+{
+    class Práctica_4_4
+    {
+        static void Main(string[] args)
+        {
+            double ventas=0.0,total=0.0;
+            int contador=0,cantidad=0;
+
+            
+            Console.WriteLine("Ingresa la cantidad de ventas");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            while (cantidad !=-1 && cantidad > 0)
+            {
+                for (int i = 1; i <=cantidad; i++)
+                {
+                    Console.WriteLine("Ingrese el monto de la venta {0}",i);
+                    ventas = Convert.ToDouble(Console.ReadLine());
+                    if (ventas !=1 && ventas>0)
+                    {
+                        total += ventas;
+                        
+                    }
+                    else i = cantidad;
+                }
+                if (total >= 0)
+                {
+                    Console.WriteLine("Total de ventas: {0:C2}", total);
+                    Console.WriteLine("Total + 5% de bono: {0:C2}", total + (total * 0.05));
+                    Console.ReadKey();
+                }
+                
+                cantidad = -2; 
+            }
+            if (cantidad == 0)
+            {
+                Console.WriteLine("No se realizaron ventas");
+                Console.ReadKey();
+            }
+            
+            
+        }
+    }
+}
---
+++ Práctica_4/Práctica_4/Práctica 4_6.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_4
+{
+    class Práctica_4_6
+    {
+        static void Main(string[] args)
+        {
+            int contador=1,cantidad,j=0;
+            double precio=0,total=0.0,total1=0.0,TOTAL=0.0;
+           
+            Console.WriteLine("Ingrese la cantidad de Sucursales:");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            
+            while (cantidad != 0)
+            {
+
+                do
+                {
+                    for (int i = 1; i <= cantidad; i++)
+                    {
+                        for (j = 1; j <= 3; j++)
+                        {
+                            Console.WriteLine("Ingrese el monto de la venta #{0} de la sucursal #{1}", j, i);
+                            precio = Convert.ToDouble(Console.ReadLine());
+                            if (precio == 0)
+                            {
+                                j = 4;
+                                i = cantidad + 1;
+                            }
+                            if (j >= 1 && j <= 3)
+                            {
+                                total += precio;
+                            }
+                        }
+                        TOTAL += total;
+                        total1 = total / 3;
+                        
+                        Console.WriteLine("\nTotal sucuarsal {0}: {1:C2}", i,total);
+                        Console.WriteLine("Promedio sucursal {0}: {1:C2}\n", i,total1);
+                    }
+                    contador=contador-2;
+                } while (contador != -1);
+                Console.WriteLine("Total global: {0:C2}",TOTAL);
+                TOTAL /= (cantidad*3);
+                Console.WriteLine("Promedio global: {0:C2}", TOTAL);
+                cantidad = 0;
+            }
+            
+            Console.ReadKey();
+            
+
+           
+            
+        }
+    }
+}
